13cb19c4c2c78b7f58310f17476d6228eb268221
kuroligergo <121298215+kuroligergo@users.noreply.github.com>
2023-01-09 19:25:58 +0100

    Added manual delete

--- kotos_app_adelnak.py
+++ kotos_app_adelnak.py
@@ -8,16 +8,16 @@ from tkinter  import *
 import tkinter as tk
 
 count=0
-num_events=20 # Ennyi sort tudsz rögzíteni a kötésedről
-        
+num_events=21 # Ennyi sort tudsz rögzíteni a kötésedről
+
 def sima(event):
     global num_events
     myLabel=Label(frame,text="Sima")
     myLabel.config(bg= "pink", fg= "white",font = "Verdana 10 bold")
     myLabel.pack()
-    count=len(frame.winfo_children()[1:])  
+    count=len(frame.winfo_children()[2:])  
     if count == num_events:                    # ez a feltétel megnézi, hogy mennyi adatot rögzítettél
-        for item in frame.winfo_children()[1:]:
+        for item in frame.winfo_children()[2:]:
             item.destroy()
         
 def fordított(event):
@@ -25,28 +25,30 @@ def fordított(event):
     myLabel=Label(frame,text="Fordított")
     myLabel.config(bg= "skyblue", fg= "black",font = "Verdana 10 bold")
     myLabel.pack()
-    count=len(frame.winfo_children()[1:])
+    count=len(frame.winfo_children()[2:])
     if count == num_events:
-        for item in frame.winfo_children()[1:]:
+        for item in frame.winfo_children()[2:]:
             item.destroy()
 
-def total(event):
-    print(count)
-    print(num_events)
-    print(frame.winfo_children()[1:])
-    print()
+def clear_frame(event):
+   for item in frame.winfo_children()[2:]:
+        item.destroy()
+
+
 
 root = Tk()
 root.geometry("800x600")
 frame =Frame(root,width=800,height=600)
 frame.pack()
 
+init=Label(frame,text="Bal egérklikk - Sima \n Billentyűzet - Fordított \n Enter - Törlés")
+init.pack()
 
 myButton=Button(frame,text="Elkezdek kötni!")
 myButton.bind("<Button-1>",sima) 
 myButton.pack(pady=20)
 myButton.bind("<Key>",fordított)
-myButton.bind("<Return>",total)
+myButton.bind("<Return>",clear_frame) # Button for manual deletion
 
 
 root.mainloop()
